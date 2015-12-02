e8cc18d7e2a6beae7e1d61aed5a0a8616c561ccf
etiennegithub <etienne.gobion@ynov.com>
2015-12-02 15:17:40 +0100

    Moving Down Plateform

---
+++ Assets/PlateformFall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlateformFall : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		rb2d.gravityScale = 0.01f;
+	}
+}
