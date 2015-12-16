4c6288d0d472eb07500f6306b27e9aa9c72656ca
Elvonnia <kimberley.ohana@ynov.com>
2015-12-16 15:45:52 +0100

    Test gameover script

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Relance ou quitte la partie en cours
+/// </summary>
+public class GameOver : MonoBehaviour
+{
+    void OnGUI()
+    {
+        const int buttonWidth = 120;
+        const int buttonHeight = 60;
+
+        if (
+          GUI.Button(
+            // Centré en x, 1/3 en y
+            new Rect(
+              Screen.width / 2 - (buttonWidth / 2),
+              (1 * Screen.height / 3) - (buttonHeight / 2),
+              buttonWidth,
+              buttonHeight
+            ),
+            "Try again ?"
+          )
+        )
+        {
+            // Recharge le niveau
+            Application.LoadLevel("test-kim");
+        }
+
+        if (
+          GUI.Button(
+            // Centré en x, 2/3 en y
+            new Rect(
+              Screen.width / 2 - (buttonWidth / 2),
+              (2 * Screen.height / 3) - (buttonHeight / 2),
+              buttonWidth,
+              buttonHeight
+            ),
+            "Retour au menu"
+          )
+        )
+        {
+            // Retourne au menu
+            Application.LoadLevel("menu");
+        }
+    }
+}
+
