e0bab9fa23e8a2cc8d7987235b3f3e577de827e7
Noki Dev <noki.dev@gmail.com>
2015-12-16 14:40:07 +0100

    Ascenceur !

--- Assets/Mecanism/MecanismScript.cs
+++ Assets/Mecanism/MecanismScript.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MecanismScript : MonoBehaviour {
+public abstract class MecanismScript : MonoBehaviour {
 
-    private bool m_Locked; //Défini si l'activation du mécanisme est bloqué ou non
-    private bool m_Activated; // Défni si le mécanisme est activé ou non
-
-    public BehaviourMecanism behaviour;
+    [SerializeField]
+    protected bool m_Locked; //Défini si l'activation du mécanisme est bloqué ou non
+    protected bool m_Activated; // Défni si le mécanisme est activé ou non
+    protected string behaviour; // Etat qui défini le comportement du mécanism
+    [SerializeField]
+    protected bool m_Auto; //Défini si le mécanisme est automatique ou non ou s'il nécessite un mecanisme d'activation (levier, bouttons etc..)
 
     public void setActivated(bool flag)
     {
@@ -15,18 +17,4 @@ public class MecanismScript : MonoBehaviour {
             m_Activated = flag;
         }
     }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (m_Activated)
-        {
-            behaviour.update();
-        }
-	}
-
 }
--- Assets/Mecanism/OpenCloseMecanism.cs
+++ Assets/Mecanism/OpenCloseMecanism.cs
@@ -5,29 +5,36 @@ public class OpenCloseMecanism : MonoBehaviour {
 
     // Use this for initialization
     public bool m_Open = false;
+    private bool triggerPlayer = false;
 
     public GameObject m_Mecanism;
 
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        MecanismScript script= m_Mecanism.GetComponent<MecanismScript>()
-            script.setActivated(m_Open);
-	}
+        if (triggerPlayer)
+        {
+            if (Input.GetKeyUp(KeyCode.Return))
+            {
+                print("KEY DOWN");
+                m_Open = !m_Open;
+            }
+        }
+        MecanismScript script = m_Mecanism.GetComponent<MecanismScript>();
+        script.setActivated(m_Open);
+    }
 
-    void OnTriggerEnter(Collider2D coll){
+    void OnTriggerEnter2D(Collider2D coll){
         
 
     }
 
-    void OnTriggerStay(Collider2D coll){
+    void OnTriggerStay2D(Collider2D coll){
         if(coll.gameObject.tag == "Player"){
-            if(Input.GetKey("Enter")){
-                m_Open = !m_Open;
-            }
+            triggerPlayer = true;
         }
     }
 }
