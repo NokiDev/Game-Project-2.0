f820db25c22a79e6a8f664170f68024f7350768d
Noki Dev <noki.dev@gmail.com>
2016-04-27 13:41:50 +0100

    End Menu and endTriggerScript added

---
+++ Assets/Scripts/MenuScripts/EndScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScript : MonoBehaviour {
+
+    const int buttonWidth = 120;
+    const int buttonHeight = 60;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (
+          GUI.Button(
+            // Centré en x, 1/3 en y
+            new Rect(
+              Screen.width / 2 - (buttonWidth / 2),
+              (1 * Screen.height / 3) - (buttonHeight / 2),
+              buttonWidth,
+              buttonHeight
+            ),
+            "Quitter"
+          )
+        )
+        {
+            Application.Quit();
+        }
+
+    }
+}
--- Assets/Scripts/MenuScripts/MenuScript.cs
+++ Assets/Scripts/MenuScripts/MenuScript.cs
@@ -13,11 +13,15 @@ public class MenuScript : MonoBehaviour
         const int buttonHeight = 60;
 
         // Affiche un bouton pour démarrer la partie
-         if (GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2),(2 * Screen.height / 3) - (buttonHeight / 2),buttonWidth,buttonHeight),"Commencer la démo !"))
+         if (GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2),(2 * Screen.height / 4) - (buttonHeight / 2),buttonWidth,buttonHeight),"Commencer la démo !"))
          {
              // Sur le clic, on démarre le premier niveau
              // "Stage1" est le nom de la première scène que nous avons créés.
              SceneManager.LoadScene("TEST/Eti/test");
          }
+        if (GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2), (2 * Screen.height / 3) - (buttonHeight / 2), buttonWidth, buttonHeight), "Quitter"))
+        {
+            Application.Quit();
+        }
     }
 }
